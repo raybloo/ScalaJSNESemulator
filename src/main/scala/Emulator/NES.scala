6252b4d103ccc739410e19b6028c721637192797
Saskia Reiss <nyotheneko@gmail.com>
2017-03-19 14:26:50 +0100

    Added basic architecture + scala doc.

--- src/main/scala/Emulator/NES.scala
+++ src/main/scala/Emulator/NES.scala
@@ -1,6 +1,10 @@
 package Emulator
 
+/** Class permitting to initialise, start and reset the emulator. 
+  * Object instance will be called and used by almost all other classes.
+  */
 class NES() {
+  // Init. all instances used for the emulator
   var cpu: CPU = new CPU(this)
   var program: Program = null
 
--- src/main/scala/Emulator/Program.scala
+++ src/main/scala/Emulator/Program.scala
@@ -1,5 +1,8 @@
 package Emulator
 
+/** TODO : Class explanation
+  * @raybloo : is it a mappers equivalent ? If not, we may have to review architecture a bit.
+  */
 class Program(nes: NES) {
 
   def load(data: Int): Unit = {
