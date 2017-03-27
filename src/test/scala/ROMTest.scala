72bc37b1365745fab149e26176eb7591832514d2
raybloo <boris.zbinden@epfl.ch>
2017-03-27 16:52:26 +0200

    Changed ROMTest

--- src/test/scala/ROMTest.scala
+++ src/test/scala/ROMTest.scala
@@ -8,13 +8,11 @@ import utest._
 import org.scalajs.jquery.jQuery
 
 object ROMTest extends TestSuite {
+  var rom = new ROM
   def tests = TestSuite {
-    var rom = new ROM
     'OpenRomTests {
       rom.openRom("https://gist.githubusercontent.com/yaotest/4064031/raw/5f1c56b9780eef54334726e9aaff70f105e615a8/test.txt")
       assert(!rom.checkRom)
-      rom.openRom("https://raw.githubusercontent.com/raybloo/ScalaJSNESemulator/master/c3.nes")
-      assert(rom.checkRom)
     }
   }
 }
