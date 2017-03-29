4de62e6a9c4b17e4d3b91c13659c48778e516f24
raybloo <boris.zbinden@epfl.ch>
2017-03-29 19:27:18 +0200

    Changed ROMTests

--- src/test/scala/ROMTest.scala
+++ src/test/scala/ROMTest.scala
@@ -4,29 +4,40 @@
 
 import Emulator.ROM
 import org.scalajs.jquery.jQuery
-import utest._
+
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
 
 object ROMTest extends TestSuite {
   var rom = new ROM
   def tests = TestSuite {
     'OpenRomTests {
       var f: Future[Any] = rom.openRom("https://gist.githubusercontent.com/yaotest/4064031/raw/5f1c56b9780eef54334726e9aaff70f105e615a8/test.txt")
-      Await.result(f,Duration.Inf)
-      assert(!rom.checkRom)
+      f.onComplete {
+        case Success(x) => assert(!rom.checkRom)
+        case Failure(e) => e.printStackTrace()
+      }
+
       f = rom.openRom("https://raw.githubusercontent.com/raybloo/ScalaJSNESemulator/master/c3.nes")
-      Await.result(f,Duration.Inf)
-      assert(rom.checkRom)
+      f.onComplete {
+        case Success(x) => assert(rom.checkRom)
+        case Failure(e) => e.printStackTrace()
+      }
     }
-    'HeaderTest {
+    'HeaderTests {
       val f: Future[Any] = rom.openRom("https://raw.githubusercontent.com/raybloo/ScalaJSNESemulator/master/c3.nes")
-      Await.result(f,Duration.Inf)
-      assert(rom.getMapperName == "Nintendo MMC5")
-      assert(rom.getMirroringType == 1)
-      assert(!rom.hasTrainer)
-      assert(rom.getChrRomSize == 16)
-      assert(rom.getPrgRomSize == 16)
+      f.onComplete {
+        case Success(x) =>
+          assert(rom.checkRom)
+          assert(rom.getMapperName == "Nintendo MMC5")
+          assert(rom.getMirroringType == 1)
+          assert(!rom.hasTrainer)
+          assert(rom.getChrRomSize == 16)
+          assert(rom.getPrgRomSize == 16)
+        case Failure(e) =>
+          e.printStackTrace()
+      }
     }
   }
 }
